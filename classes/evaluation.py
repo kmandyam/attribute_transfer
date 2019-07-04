942156ccae50fc67d65f88d5ce99bcf810f0512b
KARISHMA RAMESH MANDYAM <krm28@cs.washington.edu>
2019-07-03 22:38:01 -0700

    adds code to run predictor over test examples

--- classes/evaluation.py
+++ classes/evaluation.py
@@ -4,9 +4,10 @@
 
 from typing import List, Dict
 import os
+from allennlp.models import Model
+from allennlp.predictors.predictor import Predictor
 
 # functions to write
-# TODO: run model predictor on each line of file
 # TODO: aggregate outputs and log them
 # TODO: calculate NLTK BLEU score
 # TODO: calculate BLEU score the way Li et al. did it
@@ -22,6 +23,25 @@ def read_test_file(test_file_name: str) -> List[Dict[str, str]]:
                           "target": sentences[1]}
             test_data.append(test_datum)
     return test_data
+
+# TODO: run model predictor on each line of file
+def predict_outputs(model: Model,
+                    test_data: List[Dict[str, str]],
+                    predictor: Predictor,
+                    tgt_attr: str) -> List[Dict[str, str]]:
+    assert tgt_attr in ["positive", "negative"]
+    outputs = []
+    for datum in test_data:
+        predictions = predictor.predict(datum["source"], tgt_attr)['predictions']
+        tokens = [model.vocab.get_token_from_index(i, 'tokens') for i in predictions]
+        predicted_sentence = " ".join(tokens)
+        evaluation = {
+            "input": datum["source"],
+            "prediction": predicted_sentence,
+            "gold": datum["target"]
+        }
+        outputs.append(evaluation)
+    return outputs
 
 
 test_file = os.path.join(os.path.dirname(__file__), '../data/reference.neg.pos')
