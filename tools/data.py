d9c4511e6148bd3d9d0fbb2a1815268e6349b9c8
KARISHMA RAMESH MANDYAM <krm28@cs.washington.edu>
2019-09-07 16:41:23 -0700

    starting work on the denoising operator

--- tools/data.py
+++ tools/data.py
@@ -1,7 +1,9 @@
 from typing import List, Dict, Tuple
 from tools.generate_attributes import retrieve_attribute_vocabulary
 from nltk import ngrams
+import random
 
+random.seed(1)
 negative_vocab, positive_vocab = retrieve_attribute_vocabulary()
 
 def get_content(sentence: List[str], attribute: str) -> Tuple[List[str], List[str]]:
@@ -16,7 +18,8 @@ def get_content(sentence: List[str], attribute: str) -> Tuple[List[str], List[st
 
     attr_vocab = negative_vocab if attribute is "negative" else positive_vocab
     content, attribute_markers = remove_markers(sentence, attr_vocab)
-    return content, attribute_markers
+    denoised_markers = denoise_markers(attribute_markers, attr_vocab)
+    return content, denoised_markers
 
 def remove_markers(sentence: List[str], attr_vocab: Dict[str, float]) -> Tuple[List[str], List[str]]:
     # generate all ngrams for the sentence
@@ -40,3 +43,36 @@ def remove_markers(sentence: List[str], attr_vocab: Dict[str, float]) -> Tuple[L
             attribute_markers.append(marker)
             content = content.replace(marker, "")
     return content.split(), attribute_markers
+
+def denoise_markers(markers: List[str], attr_vocab: Dict[str, float]) -> List[str]:
+    """
+    For each attribute marker, replace with another attribute marker of the same attribute
+    and word level edit distance 1 if it exists, with probability 0.1
+    :param markers: A list of attribute markers
+    :return: A list of attribute markers with noise applied
+    """
+    for i, marker in enumerate(markers):
+        # with probability 0.1
+        if random.random() < 0.1:
+            # if there exists an attribute marker of edit distance 1, swap it
+
+def get_denoised_marker(marker: str, attr_vocab: Dict[str, float]) -> str:
+    # naively, we could loop thorugh all the attribute markers
+    # collect all the ones with edit distance 1
+    # and then choose the highest one
+    # it's preprocessing, so maybe it doesn't matter how fast?
+
+    # lowercase, tokenize and then do set union
+    # size of the union should be one off from the size of the original attribute marker
+    # to optimize, we don't consider attribute markers of length greater than marker + 1
+    # or smaller than marker - 1
+    # TODO: lowercase
+
+    marker_split = marker.split(" ")
+    for attr, score in attr_vocab.items():
+        attr_split = attr.split(" ")
+        # if there is a possibility of word edit distance, consider it
+        if len(attr_split) >= len(marker_split) - 1 and len(attr_split) <= len(marker_split) + 1:
+
+
+
